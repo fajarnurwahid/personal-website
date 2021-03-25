44835a1a8286634f43286d2b7e178e7dcad608a5
Fajar Nur Wahid <64387801+fajarstd@users.noreply.github.com>
2021-03-25 21:31:03 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(()=> {
+	$(window).scroll(()=> {
+		if (this.scrollY > 500) {
+			$('.go-up').addClass('active');
+		} else {
+			$('.go-up').removeClass('active');
+		}
+	});
+
+	$('#menu').click(()=> {
+		$('nav ul.menu').toggleClass('active');
+	});
+
+	$('.go-up').click(()=> {
+		$('html').animate({scrollTop:0});
+	});
+});
+
+
+var options = {
+	strings: ['Web Developer', 'Python Developer', 'Data Scientist', 'Logo Designer'],
+	typeSpeed: 100,
+	backSpeed: 80,
+	loop: true,
+};
+
+var typed = new Typed('.typing', options);
+var typed2 = new Typed('.typing2', options);
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+    	location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    	&& 
+    	location.hostname == this.hostname
+    	) {
+      // Figure out element to scroll to
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+        	scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+          	return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+        };
+    });
+    }
+}
+});
+
+
